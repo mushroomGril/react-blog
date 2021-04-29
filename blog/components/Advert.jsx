b30496ded8ce747dfa4c6c3f3375e0c0970b44d2
mushroomGril <1598073703@qq.com>
2021-04-29 18:15:14 +0800

    广告组件文件

---
+++ blog/components/Advert.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Advert extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Advert
